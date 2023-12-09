abfe8b154fb61fdde2601f3af438982cfec59fdf
VarinCode <warinsaipanya471@outlook.co.th>
2023-12-09 22:31:16 +0700

    Add functions and examples to function.R

--- tutorial/function.R
+++ tutorial/function.R
@@ -1,3 +1,41 @@
-myfunc <- function(){
-    4
+say_hello <- function(){
+    print("Hello")
 }
+
+say_hello()
+
+add <- function(x, y){
+    x + y
+}
+
+substract <- function(x, y){
+    x - y
+}
+
+multiply <- function(x, y){
+    x * y
+}
+
+divide <- function(x, y){
+    x / y
+}
+
+pow <- function(x, y) x ^ y
+
+n1 <- 2
+n2 <- 5
+
+print(add(x = n1, y = n2)) # 7
+print(substract(x = n1, y = n2)) # -3
+print(multiply(x = n1, y = n2)) # 10
+print(divide(x = n1, y = n2)) # 0.4
+print(pow(x = n1, y = n2)) # 32
+
+
+range <- function(start , stop){
+    for(i in start:stop){
+        print(i)
+    }
+}
+
+range(1 , 10)

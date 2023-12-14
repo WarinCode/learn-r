7ae25cc66e6309e42a812da05d356d87afd38c55
VarinCode <warinsaipanya471@outlook.co.th>
2023-12-14 14:15:32 +0700

    Add new RData file and learn.R script, print subsets of dataframe

--- week2/main5.R
+++ week2/main5.R
@@ -1,8 +1,11 @@
 df <- read.csv(file = "C:\\Users\\ACER USER5949486\\Desktop\\learn-r\\week2\\education.csv")
 
 View(df)
+
 print(df[4])
 print(df[2])
+print(df[3,])
 print(df[c(3:5)])
 print(df[c(3:5),])
-print(df[c(5,7,8), c(1,5)])
+print(df[c(5,7,8),])
+print(df[c(5, 7, 8), c(1, 5)])
